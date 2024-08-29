cef22657228efc6b8627a94b6395d4ab38711b45
Charnjeet Sohal <charnjeetsohal@outlook.com>
2024-08-29 18:08:32 +0100

    created a woking image carousel for nc-news

--- src/app/projects/nc-news/page.tsx
+++ src/app/projects/nc-news/page.tsx
@@ -1,8 +1,19 @@
+import Carousel from '@/app/ui/components/carousel';
+
 export default function Ncnews() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold">NC News</h1>
-        <p className="text-gray-700">Details about Project 1...</p>
-      </div>
-    );
-  }
+  const images = [
+    '/ui/pictures/Screenshot(149).png',  
+    '/ui/pictures/Screenshot(150).png',
+    '/ui/pictures/Screenshot(151).png',
+    '/ui/pictures/Screenshot(152).png',
+    '/ui/pictures/Screenshot(152).png',
+  ];
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">NC News</h1>
+      <p className="text-gray-700">Details about Project 1...</p>
+      <Carousel images={images} />
+    </div>
+  );
+}
