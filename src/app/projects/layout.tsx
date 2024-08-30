f552309e590d5b6556ecc5cabdb12e3b0366e4d8
Charnjeet Sohal <charnjeetsohal@outlook.com>
2024-08-30 16:28:56 +0100

    This commit will add the project descriptions as well as some basic css

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -17,7 +17,7 @@ export default function ProjectsLayout({
 }) {
   return (
     <div className={`${inter.className} bg-gray-100`}>
-      <h1>Projects</h1>
+      <h1 className="text-5xl font-extrabold bg-gradient-to-r from-blue-500 via-blue-400 to-cyan-500 text-transparent bg-clip-text mt-8 mb-6 text-center">Projects</h1>
       <div className="max-w-4xl mx-auto p-4">
         <div className="flex space-x-4 mb-4">
           <ProjectButton href="/projects/nc-news">Nc News</ProjectButton>
